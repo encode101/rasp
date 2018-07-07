73192695086798cac1a927ead02903259fa26a42
rahul mishra <rahulmishra.hello@gmail.com>
2018-07-08 00:13:24 +0530

    interim commit

--- led/led.py
+++ led/led.py
@@ -2,10 +2,12 @@
 import RPi.GPIO as IO            # calling header file for GPIO’s of PI
 import time                              # calling for time to provide delays in program
 
+#def act
+
 IO.setmode (IO.BOARD)       # programming the GPIO by BOARD pin numbers, GPIO21 is called as PIN40
 IO.setup(40,IO.OUT)             # initialize digital pin40 as an output.
 IO.output(40,1)                      # turn the LED on (making the voltage level HIGH)
-time.sleep(1)                         # sleep for a second
+time.sleep(30)                         # sleep for a second
 IO.cleanup()                         # turn the LED off (making all the output pins LOW)
 # time.sleep(1)                        #sleep for a second    
 
