c7cd383f356650ed4820dca7b103d07b9fb13441
rahul mishra <rahulmishra.hello@gmail.com>
2018-07-07 23:53:53 +0530

    interim commit

--- led/led.py
+++ led/led.py
@@ -4,23 +4,23 @@ import time                              # calling for time to provide delays in
 
 IO.setmode (IO.BOARD)       # programming the GPIO by BOARD pin numbers, GPIO21 is called as PIN40
 IO.setup(40,IO.OUT)             # initialize digital pin40 as an output.
-IO.output(40,2)                      # turn the LED on (making the voltage level HIGH)
+IO.output(40,1)                      # turn the LED on (making the voltage level HIGH)
 time.sleep(1)                         # sleep for a second
 IO.cleanup()                         # turn the LED off (making all the output pins LOW)
-time.sleep(1)                        #sleep for a second    
+# time.sleep(1)                        #sleep for a second    
 
-#loop is executed second time        
-IO.setmode (IO.BOARD)
-IO.setup(40,IO.OUT)
-IO.output(40,1)
-time.sleep(1)
-IO.cleanup()
-time.sleep(1)
+# #loop is executed second time        
+# IO.setmode (IO.BOARD)
+# IO.setup(40,IO.OUT)
+# IO.output(40,1)
+# time.sleep(1)
+# IO.cleanup()
+# time.sleep(1)
 
-#loop is executed third time
-IO.setmode (IO.BOARD)
-IO.setup(40,IO.OUT)
-IO.output(40,1)
-time.sleep(1)
-IO.cleanup()
-time.sleep(1)
+# #loop is executed third time
+# IO.setmode (IO.BOARD)
+# IO.setup(40,IO.OUT)
+# IO.output(40,1)
+# time.sleep(1)
+# IO.cleanup()
+# time.sleep(1)
